fa377ddb734f26c4489b5487485b39642bdce482
ImSRJ <surajnaithani70@gmail.com>
2023-10-16 20:15:34 +0530

    Create New App basket -
    1. created new app basket(cart functionality)
    2. user can add, delete, updated the items in the cart
    3. updated the required files

--- core/settings.py
+++ core/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "store",
+    "basket",
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "store.context_processors.categories",
+                "basket.context_processors.basket",
             ],
         },
     },
@@ -118,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+# STATICFILES_DIRS = BASE_DIR / "static/"
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
+    path("basket/", include("basket.urls")),
 ]
 
 if settings.DEBUG:
