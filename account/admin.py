06a602941a0b910a044eb4b5dde9463a0a933622
ImSRJ <surajnaithani70@gmail.com>
2023-10-20 22:33:32 +0530

    Updated account/admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+
+# Reference : UserAdmin class [ https://github.com/django/django/blob/main/django/contrib/auth/admin.py ]
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    # Controls which fields are displayed on the change list page of the admin
+    list_display = [
+        "id",
+        "email",
+        "username",
+        "is_staff",
+        "is_superuser",
+    ]
